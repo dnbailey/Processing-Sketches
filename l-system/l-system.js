a9c0fe9f5d54d8dd68ee51f8ff76c4c60c0a5282
dnbailey <dev@dnbailey.org>
2017-06-19 09:39:25 -0400

    Add axiom and rules

--- l-system/l-system.js
+++ l-system/l-system.js
@@ -1,3 +1,20 @@
+// axiom: A
+let axiom = "A"
+
+// rules: (A → +B−A−B+), (B → −A+B+A−)
+let rules = [
+  {
+    input: 'A',
+    result: '+B-A-B+'
+  },
+  {
+    input: 'B',
+    result: '-A+B+A-'
+  }
+]
+
+console.log(rules)
+
 function setup () {
   createCanvas(600,500)
 }

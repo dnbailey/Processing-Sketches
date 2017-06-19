7fbc7d26157ebdab41f3ce75405a312b4389f83a
dnbailey <dev@dnbailey.org>
2017-06-18 22:39:39 -0400

    Create canvas

--- fractal-tree/fractal-tree.js
+++ fractal-tree/fractal-tree.js
@@ -1,7 +1,15 @@
-function setup () {
+/*
+  Fractal Tree
 
+  Based on the YouTube tutorial by CodeTrain
+  https://www.youtube.com/watch?v=0jjeOYMjmDU
+
+*/
+
+function setup () {
+  createCanvas(700, 700)
 }
 
 function draw () {
-
+  background(51)
 }
